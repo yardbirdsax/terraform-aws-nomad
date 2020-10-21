8244879666b848f86894afb6cae825de3352cfa9
Josh Feierman <>
2020-10-21 15:00:50 -0400

    Updated some scripts and Packer config files.

--- examples/nomad-consul-ami/setup_ubuntu.sh
+++ examples/nomad-consul-ami/setup_ubuntu.sh
@@ -10,10 +10,5 @@ SCRIPT=`basename "$0"`
 # Using Docker CE directly provided by Docker
 echo "[INFO] [${SCRIPT}] Setup docker"
 cd /tmp/
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-apt-cache policy docker-ce
-
-sudo apt-get install -y docker-ce
+curl -fsSL https://get.docker.com | CHANNEL=stable sudo sh -
 sudo usermod -a -G docker ubuntu
